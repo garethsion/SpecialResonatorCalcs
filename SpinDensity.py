4d97fa8b6590eb1e5f960aaf8eed02278ea41ef6
Gareth Jones <garethsion@googlemail.com>
2018-11-01 09:14:04 +0000

    Trying to get figures of merit. Not quite sure I have purcell enhancement right, but maybe nnearly there

--- SpinDensity.py
+++ SpinDensity.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 import numpy.matlib
 import matplotlib.pyplot as plt
-from process_data import ReadComsol
+from process_data import ReadComsol,PostProcData
 import Plotting
 
 # Read data from downloads
@@ -29,34 +29,47 @@ ue = sp.physical_constants["Bohr magneton"][0]
 g = [*map(lambda x,y: 0.47 * ue * np.sqrt(y**2 + x**2),dbx,dby)]
 g = np.asarray([x / sp.h for x in g])
 
-# Reshape g so we can append multiple values for each box section
-g=g.reshape(len(g),1)
-np.shape(g)
+post = PostProcData.PostProcData()
 
-# Calculate the size of the boxes
-bucket = bx_x.count(bx_x[0]) # number of samples for each point in space
-x_box = abs(float(bx_x[bucket-1]) - float(bx_x[bucket]))
-y_box = abs(float(bx_y[0]) - float(bx_y[1]))
-z_box = x_box
-volume = x_box * y_box * z_box
+# hist, edges = post.spin_density(bx_x,bx_y,g)
 
-# Calculate number of spins in each cell
-no_spins_in_box = 1e7
-Ncell = round(no_spins_in_box * volume)
+# Calculate Purcell enhancement at each grid point
+Q = 10000 # Q factor - for now typed in, but will be found from CST calcs ultimately
+purcell = post.purcell_rate(g,Q)
+plt.plot(g,purcell)
+plt.show()
 
-g = np.matlib.repmat(g, 1, Ncell)
+pdens, pedge = post.purcell_density(bx_x,bx_y,purcell)
 
-# Calculate histogram
-hist, edges = np.histogram(g, bins=500) # single spin
-hist = hist * Ncell # with 3d box
-hist = hist / sum(hist) # normalize
-edges = edges[0:len(hist)] # shift bin edges to get the same length as data
+plt.bar(pdens,height=pedge,alpha=0.15)
+plt.plot(pedge,pdens)
+plt.show()
 
-plts = Plotting.Plotting()
+# plt.plot(pdens,hist)
+# plt.show()
+# fig, ax = plt.subplots(figsize=(6,4))
+# ax1 = plt.subplot(1,2,1)
+# ax1.bar(edges,height=hist,alpha=0.15)
+# ax1.plot(edges,hist)
+# ax1.set_xlabel('$\\frac{g}{2 \pi} (Hz)$',fontsize='24')
+# ax1.set_ylabel('$\\rho(g)$',fontsize='24')
+# plt.tight_layout()
 
-plt1 = plts.barplot(edges,hist,colr='b',xlab='$\\frac{g}{2 \pi} (Hz)$',
-	ylab='$\\rho(g)$',filename='g_density.eps',
-	show='yes',save='yes')
+# ax2 = plt.subplot(1,2,2)
+# #ax.bar(pedge,height=pdens,alpha=0.15)
+# ax2.plot(pedge,pdens)
+# ax2.set_xlabel('$\Gamma_{p}$',fontsize='24')
+# ax2.set_ylabel('$\\rho(g)$',fontsize='24')
+# plt.tight_layout()
+# plt.show()
+
+# plt.savefig(str(os.getcwd() + '/figs/' + filename))
+
+# plts = Plotting.Plotting()
+
+# plt1 = plts.barplot(edges,hist,colr='b',xlab='$\\frac{g}{2 \pi} (Hz)$',
+# 	ylab='$\\rho(g)$',filename='g_density.eps',
+# 	show='yes',save='no')
 
 # # n, bins, patches = plt.hist(x=d, bins='auto', color='#0504aa',
 # # 	alpha=0.7, rwidth=0.85)
--- process_data/PostProcData.py
+++ process_data/PostProcData.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python 
 import numpy as np
+import numpy.matlib
 from scipy import constants as sp
 from process_data import SetParams
 
@@ -32,13 +33,56 @@ class PostProcData:
         rho =  sp.m_e / self.volume_cell
         return rho
 
-    def purcell_rate(self,Q,*args,**kwargs):
+    def spin_density(self,x,y,g):
+        Ncell = self.ncell(x,y,g)
+
+        g = np.matlib.repmat(g, 1, Ncell)
+
+        # Calculate histogram
+        hist, edges = np.histogram(g, bins=500) # single spin
+        hist = hist * Ncell # with 3d box
+        hist = hist / sum(hist) # normalize
+        edges = edges[0:len(hist)] # shift bin edges to get the same length as data
+
+        return hist, edges
+
+    def purcell_density(self,x,y,gamma):
+        Ncell = self.ncell(x,y,gamma)
+
+        gamma = np.matlib.repmat(gamma, 1, Ncell)
+
+        # Calculate histogram
+        hist, edges = np.histogram(gamma, bins=500) # single spin
+        hist = hist * Ncell # with 3d box
+        hist = hist / sum(hist) # normalize
+        edges = edges[0:len(hist)] # shift bin edges to get the same length as data
+
+        return hist, edges
+
+    def ncell(self,x,y,param):
+        # Reshape g so we can append multiple values for each box section
+        param=param.reshape(len(param),1)
+
+        # Calculate the size of the boxes
+        bucket = x.count(x[0]) # number of samples for each point in space
+        x_box = abs(float(x[bucket-1]) - float(x[bucket]))
+        y_box = abs(float(y[0]) - float(y[1]))
+        z_box = x_box
+        volume = x_box * y_box * z_box
+
+        # Calculate number of spins in each cell
+        no_spins_in_box = 1e7
+        Ncell = round(no_spins_in_box * volume)
+        return Ncell
+
+    def purcell_rate(self,g,Q,*args,**kwargs):
         # Calculates the Purcell rate
-        k = self.omega_0 / Q
-        omega_s = kwargs.get('omega_s',omega_0)
-        delta = omega_0 - omega_s
+        k = self.omega / Q
+        omega_s = kwargs.get('omega_s',self.omega)
+        delta = self.omega - omega_s
         
-        purcell = k * ((self.g**2) / (k**2) / (4 + delta**2))
+        purcell = k * ((g**2) / (k**2) / (4 + delta**2))
+        # purcell = (4*(g**2)) / k
         return purcell
 
     def purcell_factor(self,lambda_c,n,Q):
